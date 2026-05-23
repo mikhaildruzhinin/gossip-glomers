261ec3f81e41f2f9ad6f04881552f9d1cace4e29
Mikhail Druzhinin <druzhininmy@gmail.com>
2026-05-23 18:46:12 +0300

    add echo

---
+++ scripts/echo.sh
@@ -0,0 +1,2 @@
+MAELSTROM_MAIN="ru.mikhaildruzhinin.gossipglomers.Echo" \
+bin/maelstrom/maelstrom test -w echo --bin "scripts/run.sh" --nodes n1 --time-limit 10 --log-stderr
---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+cd "$PROJECT_ROOT" || exit 1
+
+mvn -q compile dependency:build-classpath \
+  -Dmdep.outputFile=target/classpath.txt \
+  >/dev/null
+
+exec java -cp "target/classes:$(cat target/classpath.txt)" $MAELSTROM_MAIN
